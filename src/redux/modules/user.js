b1068611ea28b79cf590f9df5dbb127e571504a5
hovinee <hobinskyyy@naver.com>
2022-03-24 15:49:48 +0900

    https 수정

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -28,7 +28,7 @@ const kakaoLogin = (code) => {
   console.log(code);
   return function (dispatch, getState, { history }) {
     axios
-      .get(`http://stgon.shop/api/auth/kakao/callback?code=${code}`)
+      .get(`https://stgon.shop/api/auth/kakao/callback?code=${code}`)
       .then((res) => {
         console.log(res); // 토큰 넘어오는지 확인
         const token = res.data.token;
