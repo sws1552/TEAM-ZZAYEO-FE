23b0de8f98db0ca1da205729a4495da333c2d415
hovinee <hobinskyyy@naver.com>
2022-03-24 16:38:01 +0900

    https 수정

--- src/shared/Socket.js
+++ src/shared/Socket.js
@@ -2,4 +2,4 @@ import io from "socket.io-client";
 
 // 서버와 클라이언트 연결
 // export const socket = io.connect("http://localhost:5000");
-export const socket = io.connect("http://stgon.shop");
+export const socket = io.connect("https://stgon.shop");
